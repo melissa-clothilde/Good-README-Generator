81b90a42172d5f0d596f71a7304fbb2f9b4b6130
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-06-09 22:11:38 -0700

    delivers readme

---
+++ generatedREADME.md
@@ -0,0 +1,25 @@
+
+    # Title
+    ## Description
+    
+    ## Table of Contents
+    * [Installation](#installation) 
+    * [Usage](#usage) 
+    * [Credits](#credits) 
+    * [License](#license) 
+    * [Contributors](#contributors) 
+    ## Installation 
+     
+    ## Usage 
+     
+    ## License 
+    
+    ## Contributors 
+     
+    ## Tests 
+     
+    ## Questions
+
+    ![profile](https://avatars3.githubusercontent.com/u/62398803?v=4) 
+    
+    email@emial
--- index.js
+++ index.js
@@ -1,41 +1,112 @@
-const inquirer = require("inquire");
+const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const axios = require("axios");
 
-const writeFileAsync = util.promisify(fs.writeFile);
+writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser () {
+function promptUser() {
     return inquirer.prompt([
         {
             type: "input",
-            username: "name",
-            message: "What is your GitHub username?"
+            name: "title",
+            message: "What is your project title?"
         },
         {
-
-        }
-    ]);
+            type: "input",
+            name: "description",
+            message: "Provide your detailed project description."
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide instructions and examples for use."
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Provide a license name."
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Provide contributors."
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Write tests for your application. Then provide examples on how to run them."
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your github username"
+        },
+        {
+            type: "input",
+            name: "question2",
+            message: "What is your github email?"
+        },
+    ])
+    .then(async function (answers) {
+        const {username} = answers;
+        const queryUrl = `https://api.github.com/users/${username}`;
+        console.log(queryUrl);
+        const res = await axios.get(queryUrl);
+        console.log("res", res);
+        answers.avatar = res.data.avatar_url;
+        return answers;
+    });
 }
 
-function generatMD(answers) {
+function generateMarkdown(answers) {
     return `
- 
-  }
+    # ${answers.title}
+    ## Description
+    ${answers.description}
+    ## Table of Contents
+    * [Installation](#installation) 
+    * [Usage](#usage) 
+    * [Credits](#credits) 
+    * [License](#license) 
+    * [Contributors](#contributors) 
+    ## Installation 
+    ${answers.installation} 
+    ## Usage 
+    ${answers.usage} 
+    ## License 
+    ${answers.license}
+    ## Contributors 
+    ${answers.contributors} 
+    ## Tests 
+    ${answers.tests} 
+    ## Questions
 
-function generateMD()
+    ![profile](${answers.avatar}) 
+    
+    ${answers.question2}`;
+}
+
+module.exports = generateMarkdown;
+
 async function init() {
-  console.log("hi")
-  try {
-    const answers = await promptUser();
 
-    const html = generateHTML(answers);
+    try {
+        const answers = await promptUser();
+        console.log("answers", answers);
+        const markdown = generateMarkdown(answers);
 
-    await writeFileAsync("generatedREADME.md", html);
+        await writeFileAsync("generatedREADME.md", markdown);
 
-    console.log("Successfully wrote to index.html");
-  } catch(err) {
-    console.log(err);
-  }
+        console.log("Successfully wrote to generatedREADME.md");
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 init();
